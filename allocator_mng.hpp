433cbb30301ad147f3a5e6e170f5700d818e565f
Oleg Sazonov <whitylmn@gmail.com>
2019-11-05 20:39:43 +0300

    Update

--- allocator_mng.hpp
+++ allocator_mng.hpp
@@ -2,10 +2,23 @@
 #define  typename template<typename T>
 #include <iostream>
 #include <stdlib.h>
+#include <sys/mman.h>
 using namespace std;
 
 // Allocator wrapper's namespace
 namespace alloc {
+namespace linux {
+  typename int smart_mlock(T *&pptr, const size_t size) {
+    return mlock((T*)pptr, size);
+  }
+  typename int smart_munlock(T* &pptr, const size_t size) {
+    return munlock((T*)pptr, size);
+  }
+  typename T* smart_mremap(T* old, const size_t old_size, const size_t new_size, int flags) {
+    return (T*)mremap(old, old_size, new_size, flags);
+  }
+  
+} // namespace linux
 /* Очищает память и убирает эффект "висячего" указателя */
 typename void _free(T *&pptr) {
   free(pptr);
@@ -15,7 +28,7 @@ typename void _free(T *&pptr) {
  * Аллоцирует память по размеру типа + некоторый запас
  * @param add Запас
  */
-typename T *smart_alloc(int add = 0) {
+typename T *smart_alloc(const int add = 0) {
   return (T *)malloc(sizeof(T) + add);
 }
 /*
@@ -23,14 +36,14 @@ typename T *smart_alloc(int add = 0) {
  * @param number кол-во блоков
  * @param add Запас памяти
  */
-typename T *smart_calloc(int number = 0, int add = 0) {
+typename T *smart_calloc(const int number,  int add = 0) {
   return (T *)calloc(number, sizeof(T) + add);
 }
 
 /*
  * Алоцирует память, выровненую по alignement размером типа + add
  */
-typename T *smart_alignedalloc(size_t alignement, size_t add = 0) {
+typename T *smart_alignedalloc(const size_t alignement, size_t add = 0) {
   return (T *)aligned_alloc(alignement, sizeof(T) + add);
 }
 
